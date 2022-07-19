a4b734a2904da330c92b190f26ebc74af8eba8da
hieuga1111 <dmh29112000@gmail.com>
2022-07-19 10:12:46 +0700

    hieu

--- GetLink/constants.js
+++ GetLink/constants.js
@@ -24,6 +24,33 @@ const INFORMATION_PIKBEST = {
     DOWNLOAD_ELEMENT: "a.block-gradient.graHover.dlbtn.ga-click"
     
 };
+const INFORMATION_PNGTREE = {
+    DOMAIN: "https://vi.pngtree.com/",
+    ACCOUNT: "vuongle.201096@gmail.com",
+    PASSWORD: "rollie3011",
+    DOWNLOAD: [
+        {
+            DOWNLOAD_TYPE: 0,
+            DOWNLOAD_NAME: "PNG",
+            DOWNLOAD_ELEMENT: ".btn-downjpg",
+            DOWNLOAD_RESOURCE: [
+                "https://proxy-rar.pngtree.com/"
+            ]
+        },
+        {
+            DOWNLOAD_TYPE: 1,
+            DOWNLOAD_NAME: "EPS",
+            DOWNLOAD_ELEMENT: ".btn-downeps",
+            DOWNLOAD_RESOURCE: [
+                "https://proxy-rar.pngtree.com",
+                "https://proxy-t"
+            ]
+        }
+    ],
+    DOWNLOAD_ELEMENT: ".btn-downeps"
+    
+};
 module.exports = {
-    INFORMATION_PIKBEST
+    INFORMATION_PIKBEST,
+    INFORMATION_PNGTREE
 }
